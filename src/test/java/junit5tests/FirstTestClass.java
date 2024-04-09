bc012513462d5069df76aeb2dc39a0cca5e60387
Prokop Ferdan <prokes1@gmail.com>
2024-04-09 08:46:09 +0200

    added lifecycle methods

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,9 +1,28 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll(){
+        System.out.println("..This is the before all method");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("----This is before each method");
+    }
+
+    @AfterAll
+    void afterAll(){
+        System.out.println("..This the after all method");
+    }
+    @AfterEach
+    void afterEach(){
+        System.out.println("----This is after each method");
+    }
 
     @Test
     void firstMethod(){
